a27c7db8a80e5bf3bada1d1ee5cee165a3110fc7
Krushna Sahu <71935479+crypticbeing@users.noreply.github.com>
2024-09-02 09:59:05 +0000

    new commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const accountId = "123345"
+let accountEmail = "sahukrushna679@gmail.com"
+var accountPass = "Dunkirk#456"
+accountCity = "Jaipur"
+
+//accountId = "2" //not allowed
+
+accountEmail ="hs@hs.com"
+accountPass = "1dr56t"
+accountCity = "pass"
+
+console.table([accountId,accountEmail ,accountPass , accountCity])
+
+/* Prefer not to use 'var' because fo bloack scope and function scope */
---
+++ 02_datatyoes.js
@@ -0,0 +1,23 @@
+/*Primitive data type and Reference data type*/
+
+//"use strict"; -- used for strictly using new versions of javascript
+
+//alert(3+3) // we are using node js not the browser to run the javascript, this is a browser feature of javascript.
+
+/*console.log(3+
+    3)//code readability also you can use this thing on your resume */
+
+
+//number = 2 to power of 53
+//string = "ghty"
+//null = empty always
+//undefined means = we have declase the variable but not assigned any value
+//boolean = true or false
+//bigint = used in projects like developing stock market websites where big numbers are required
+//symbols = || and && and (++,--, ) 
+
+
+// array = many values stored in a single varibale
+//object = properties of a variable
+
+console.table([typeof null, typeof undefined])// 'object' , 'undefined'
